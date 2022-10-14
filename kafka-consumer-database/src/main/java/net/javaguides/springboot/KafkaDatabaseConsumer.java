254e2392c96b0f158459f553661ebf6966f384fb
denys <laptev.denis@mail.ru>
2022-10-14 19:05:15 +0300

    Kafka and SpringBoot project. Flow: Wikimedia - Producer - Kafka - Consumer - Database (MySQL)

---
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
@@ -0,0 +1,31 @@
+package net.javaguides.springboot;
+
+import net.javaguides.springboot.entity.WikimediaData;
+import net.javaguides.springboot.repository.WikimediaDataRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDatabaseConsumer {
+    public static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    private WikimediaDataRepository dataRepository;
+
+    @Autowired
+    public KafkaDatabaseConsumer(WikimediaDataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
+    @KafkaListener(topics = "wikimedia_recentchange", groupId = "my-consumer-group")
+    public void consume(String eventMessage) {
+        LOGGER.info(String.format("Event message received -> %s", eventMessage));
+
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+
+        dataRepository.save(wikimediaData);
+    }
+}
